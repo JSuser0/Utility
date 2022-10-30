54da8c497c067b61b1a12bbd35057ff2368c929f
JSuser0 <108574043+JSuser0@users.noreply.github.com>
2022-10-29 22:47:33 -0500

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -10,6 +10,8 @@ local players = game:GetService("Players")
 local client = players.LocalPlayer
 local character = client.Character
 
+local mouse = client:GetMouse()
+
 local rootPart = character:WaitForChild("HumanoidRootPart")
 
 local function Update()
@@ -44,6 +46,35 @@ function functions:GetNearestPlayer()
     end)
     
      return distances[1][1], distances[1][2]
+end
+
+function functions:GetNearestPlayerByMouse()
+    task.defer(Update)
+    
+    local distances = { };
+
+     for _, player in pairs(players:GetPlayers()) do
+         if (player.Name == client.Name or not player.Character) then continue end
+    
+         pcall(function()
+            local playerRootPart = player.Character.HumanoidRootPart
+    
+            table.insert(distances, {
+                player.Character,
+                (mouse.Hit.Position - playerRootPart.Position).Magnitude or math.huge
+            })
+        end)
+    end
+    
+    table.sort(distances, function(a, b)
+        return b[2] > a[2]
+    end)
+    
+     return distances[1][1], distances[1][2]
+end
+
+
+function functions:LogData()
 end
 
 
